fe7b9dddb2b8ec2db100ed179abbed4767cce3da
yuanhuan <yuanhuan@tcl.com>
2014-08-27 13:46:17 +0800

    Signed-off-by: yuanhuan <yuanhuan@tcl.com>

--- src/main/java/com/test/prime/Main.java
+++ src/main/java/com/test/prime/Main.java
@@ -6,40 +6,46 @@ public class Main
 {
 	public static void test(int num)
 	{
+		System.out.println("Num = " + num + ":");
 		long beginTime = 0;
 
+		/*
 		beginTime = System.currentTimeMillis();
 		List<Integer> primeAList = PrimeA.getAllPrimes(num);
 		long primeACostTime = System.currentTimeMillis() - beginTime;
+		System.out.println("PrimeA total " + primeAList.size() + " primes, Cost time: " + primeACostTime + " ms");
+		
 
 		beginTime = System.currentTimeMillis();
 		List<Integer> primeBList = PrimeB.getAllPrimes(num);
 		long primeBCostTime = System.currentTimeMillis() - beginTime;
+		System.out.println("PrimeB total " + primeBList.size() + " primes, Cost time: " + primeBCostTime + " ms");
+		
 
 		beginTime = System.currentTimeMillis();
 		List<Integer> primeCList = PrimeC.getAllPrimes(num);
 		long primeCCostTime = System.currentTimeMillis() - beginTime;
+		System.out.println("PrimeC total " + primeCList.size() + " primes, Cost time: " + primeCCostTime + " ms");
+		*/
 
 		beginTime = System.currentTimeMillis();
 		List<Integer> primeDList = PrimeD.getAllPrimes(num);
 		long primeDCostTime = System.currentTimeMillis() - beginTime;
+		System.out.println("PrimeD total " + primeDList.size() + " primes, Cost time: " + primeDCostTime + " ms");
 
 		beginTime = System.currentTimeMillis();
 		List<Integer> primeEList = PrimeE.getAllPrimes(num);
 		long primeECostTime = System.currentTimeMillis() - beginTime;
-
-		System.out.println("Num = " + num + ":");
-		System.out.println("PrimeA total " + primeAList.size() + " primes, Cost time: " + primeACostTime + " ms");
-		System.out.println("PrimeB total " + primeBList.size() + " primes, Cost time: " + primeBCostTime + " ms");
-		System.out.println("PrimeC total " + primeCList.size() + " primes, Cost time: " + primeCCostTime + " ms");
-		System.out.println("PrimeD total " + primeDList.size() + " primes, Cost time: " + primeDCostTime + " ms");
 		System.out.println("PrimeE total " + primeEList.size() + " primes, Cost time: " + primeECostTime + " ms");
-		System.out.println("");
+
 		System.out.println("");
 	}
 
 	public static void main(String[] args)
 	{
+		Main.test(1000);
+		Main.test(10000);
+		Main.test(100000);
 		Main.test(1000000);
 		Main.test(10000000);
 		Main.test(100000000);
