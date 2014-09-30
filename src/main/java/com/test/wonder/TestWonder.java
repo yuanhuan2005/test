780d6f03cbae1d4a2dcc6cf489d5dc6b7f189c3d
yuanhuan <yuanhuan@tcl.com>
2014-09-30 11:04:19 +0800

    small changes
    Signed-off-by: yuanhuan <yuanhuan@tcl.com>

--- src/main/java/com/test/wonder/TestWonder.java
+++ src/main/java/com/test/wonder/TestWonder.java
@@ -13,14 +13,10 @@ public class TestWonder
 	public static void main(String[] args) throws IOException
 	{
 		String postUrl = "http://127.0.0.1:8080/wms/api/QueryByFingerPrinting";
-		String postData = "";
-
 		String featureFilename = "D:/Download/wonder/results/smokin-aces-2.data";
 		byte[] feature = Arrays.copyOfRange(FileUtils.toByteArray(featureFilename), 0, 160);
 		System.out.println(feature.length);
-		postData = new String(feature);
-
-		CustomHttpResponse result = HttpRequestUtils.sendPostRequest(postUrl, postData);
+		CustomHttpResponse result = HttpRequestUtils.sendPostRequest(postUrl, new String(feature));
 		System.out.println(result.toString());
 	}
 }
