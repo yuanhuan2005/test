9c627a0ea901d7299c3e27dd94d7ffb18f1607f4
yuanhuan2005 <yuanhuan_2005@126.com>
2014-10-07 01:25:08 +0800

    add a skill
    Signed-off-by: yuanhuan2005 <yuanhuan_2005@126.com>

--- src/main/java/com/test/auth/Test.java
+++ src/main/java/com/test/auth/Test.java
@@ -1,11 +1,40 @@
 package com.test.auth;
 
+import java.io.IOException;
+
 public class Test
 {
 
-	public static void main(String[] args)
+	public static boolean isTwoJC(int num)
 	{
+		if (num < 2)
+		{
+			return false;
+		}
 
+		if ((num & (num - 1)) == 0)
+		{
+			return true;
+		}
+
+		return false;
 	}
 
+	public static void main(String[] args) throws IOException
+	{
+		long beginTime = System.currentTimeMillis();
+
+		boolean result = false;
+		int loop = 2100000000;
+		for (int i = 1; i <= loop; i++)
+		{
+			result = Test.isTwoJC(i);
+			if (result)
+			{
+				System.out.println(i + " : " + result);
+			}
+		}
+
+		System.out.println("Cost time: " + (System.currentTimeMillis() - beginTime));
+	}
 }
